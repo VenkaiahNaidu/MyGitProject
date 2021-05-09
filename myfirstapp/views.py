41b575eda2f1d12412f27d6b6924cb3fb80dfb2b
Venky Breadwinner <37826016+VenkaiahNaidu@users.noreply.github.com>
2021-05-09 15:51:56 +0530

    Update views.py

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def hello(reqeust):
 	return HttpResponse("welcome to django project ")
+
+def venky(reqeust):
+	return HttpResponse("this is my commit")
